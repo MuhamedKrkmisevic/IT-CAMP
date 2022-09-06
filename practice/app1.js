c5507078be056cb014bd748d6446080a98593c40
muhamedkrkmisevic <maska051@hotmail.com>
2022-09-06 23:52:57 +0200

    practice

---
+++ practice/app1.js
@@ -0,0 +1,29 @@
+let prom = 25;
+
+function nazivFunkcije() {
+  return prom;
+}
+
+console.log(nazivFunkcije());
+
+function zbirCetiriISest() {
+  return 4 + 6;
+}
+
+console.log(zbirCetiriISest());
+
+function zbirDvaBroja(prviBroj, drugiBroj) {
+  return prviBroj + drugiBroj;
+}
+
+console.log(zbirDvaBroja(12, 13));
+
+function umnozakDvaBroja() {
+  return 15 * 15;
+}
+console.log(umnozakDvaBroja());
+
+function deljenjeDvaBroja() {
+  25 % 5;
+}
+console.log(deljenjeDvaBroja());
