5d59eca656737e3d234aa75ac86794e4b1824ac7
muhamedkrkmisevic <maska051@hotmail.com>
2022-09-21 19:40:32 +0200

    omaci

---
+++ JavaScript/17.cas/domaci2.js
@@ -0,0 +1,6 @@
+function fanksn(sirina, visina) {
+  let povrsina = sirina * visina;
+  return `Povrsina pravougaonika cija je sirina ${sirina}, a visina ${visina} je ${povrsina}`;
+}
+
+console.log(fanksn(12, 16));
