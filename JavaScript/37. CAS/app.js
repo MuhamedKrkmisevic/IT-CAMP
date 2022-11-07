f3a4bbf6b8bf07142f703f518fc69a2abf4852c1
muhamedkrkmisevic <maska051@hotmail.com>
2022-11-07 20:57:35 +0100

    38. cas

---
+++ JavaScript/37. CAS/app.js
@@ -0,0 +1,12 @@
+console.log(saberiDvaBroja(4, 6));
+
+function saberiDvaBroja(br1, br2) {
+  return br1 + br2;
+}
+
+// Hoisting kod funkcija vazi ako se radi o funkcijama definisanim preko function keyword, dok kod arrow funkcija ne vazi
+//(nije moguce pozvati funkciju pre definisanja iste)
+
+console.log(!-0);
+
+const obj = {};
