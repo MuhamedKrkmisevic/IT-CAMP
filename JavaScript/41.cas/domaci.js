49e427df1b033d0bc96836cdf9b201f244f8f226
muhamedkrkmisevic <maska051@hotmail.com>
2022-11-16 20:58:50 +0100

    41.cas

---
+++ JavaScript/41.cas/domaci.js
@@ -0,0 +1,9 @@
+const vahid = new Date(2033, 5, 25);
+console.log(vahid.getDate());
+Date.parse;
+
+const amina = new Date(2003, 11, 8);
+const munevera = new Date(2003, 11, 13);
+
+const aRazlika = Date.parse();
+const mRazlika = Date.parse();
