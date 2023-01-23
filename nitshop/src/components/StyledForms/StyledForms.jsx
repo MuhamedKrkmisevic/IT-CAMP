9dc4d3e0066fd9622aa5b5c1a470a792467473d7
muhamedkrkmisevic <maska051@hotmail.com>
2023-01-23 21:00:03 +0100

    react

--- nitshop/src/components/StyledForms/StyledForms.jsx
+++ nitshop/src/components/StyledForms/StyledForms.jsx
@@ -1,33 +1,33 @@
-import React from "react";
-import "./StyledForms";
-import "./StyledForms.css";
+// import React from "react";
+// import "./StyledForms";
+// import "./StyledForms.css";
 
-function StyledForms(props) {
-  return (
-    <div>
-      <label for="name">Unesite svoje ime</label>
-      <input
-        type="text"
-        name="Ime"
-        id="name"
-        placeholder="Unesite ime"
-        required
-      ></input>
+// function StyledForms(props) {
+//   return (
+//     <div>
+//       <label for="name">Unesite svoje ime</label>
+//       <input
+//         type="text"
+//         name="Ime"
+//         id="name"
+//         placeholder="Unesite ime"
+//         required
+//       ></input>
 
-      <button type="posalji">Posalji</button>
-      <br />
-      <label for="lastname">Unesite svoje prezime</label>
-      <input
-        type="text"
-        name="Prezime"
-        id="Lastname"
-        placeholder="Unesite prezime"
-        required
-      />
+//       <button type="posalji">Posalji</button>
+//       <br />
+//       <label for="lastname">Unesite svoje prezime</label>
+//       <input
+//         type="text"
+//         name="Prezime"
+//         id="Lastname"
+//         placeholder="Unesite prezime"
+//         required
+//       />
 
-      <button type="posalji">Posalji</button>
-    </div>
-  );
-}
+//       <button type="posalji">Posalji</button>
+//     </div>
+//   );
+// }
 
-export default StyledForms;
+// export default StyledForms;
