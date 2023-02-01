8036f23c1466fba59876b9f9cd15503b047e0f0d
muhamedkrkmisevic <maska051@hotmail.com>
2023-02-01 21:00:10 +0100

    mui

--- nitshop/src/App.jsx
+++ nitshop/src/App.jsx
@@ -1,11 +1,12 @@
+import React from "react";
 import "./App.css";
-import ToDo from "./components/ToDo/ToDo";
+import GridCmp from "./components/Grid/Grid";
 
 const App = () => {
   return (
-    <div>
-      <ToDo />
-    </div>
+    <>
+      <GridCmp />
+    </>
   );
 };
 export default App;
