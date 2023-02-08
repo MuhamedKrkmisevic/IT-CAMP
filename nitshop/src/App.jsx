eb8c76eb355e195904c39e634aba42b80dd25538
muhamedkrkmisevic <maska051@hotmail.com>
2023-02-08 21:00:49 +0100

    sajt

--- nitshop/src/App.jsx
+++ nitshop/src/App.jsx
@@ -4,13 +4,12 @@ import GridCmp from "./components/Grid/Grid";
 import PrimaryButton from "./components/PrimaryButton/PrimaryButton";
 import StyledButton from "./components/StyledButton/StyledButton";
 import StyledDiv from "./components/StyledDiv/StyledDiv";
+import Navigation from "./Templates/Navigation/Navigation";
 
 const App = () => {
   return (
     <>
-      <PrimaryButton opacity={145}>Primary Button</PrimaryButton>
-      <StyledButton innerText="mule" />
-      <StyledDiv>divv</StyledDiv>
+      <Navigation></Navigation>
     </>
   );
 };
