90b6fa803dedf163081518fe0133b44947bb82f3
muhamedkrkmisevic <maska051@hotmail.com>
2023-02-15 20:58:43 +0100

    push

--- nitshop/src/Templates/Navigation/MainBar.jsx
+++ nitshop/src/Templates/Navigation/MainBar.jsx
@@ -2,11 +2,12 @@ import Grid from "@mui/material/Grid";
 import CustomDiv from "../../components/CustomDiv/CustomDiv";
 import { colors, fontSize } from "../../util/theme";
 import { getScreenWidth } from "../../util/helpers";
+import Logo from "../../assets/images/logo.png";
 
 const MainBar = () => {
   return (
     <CustomDiv
-      bgColor={colors.secondColor}
+      bgColor="white"
       display="flex"
       width="100%"
       height="30px"
@@ -14,7 +15,16 @@ const MainBar = () => {
       border="0px"
     >
       <Grid container direction="row">
-        <Grid item md={6} lg={6}></Grid>
+        <Grid item md={6} lg={6}>
+          <img
+            src={Logo}
+            alt="logo"
+            style={{
+              width: 200,
+              height: 100,
+            }}
+          />
+        </Grid>
         <Grid item md={6} lg={6}></Grid>
       </Grid>
     </CustomDiv>
