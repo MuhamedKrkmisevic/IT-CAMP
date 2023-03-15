e9cee31b7c82a8f509249dc78e4f8dbc92247783
muhamedkrkmisevic <maska051@hotmail.com>
2023-03-15 20:57:43 +0100

    pushaj

--- nitshop/src/Templates/ArticleCard/ArticleCard.jsx
+++ nitshop/src/Templates/ArticleCard/ArticleCard.jsx
@@ -2,25 +2,32 @@ import React from "react";
 import Text from "../../components/Text/Text";
 import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
 import { Card } from "@mui/material";
-import { fontSize, colors } from "../../util/theme";
+import { fontSize, colors, fontWeight } from "../../util/theme";
 import PrimaryButton from "../../components/PrimaryButton/PrimaryButton";
+import ShoppingCartOutlined from "@mui/icons-material/ShoppingCartOutlined";
 
 const ArticleCard = ({ title, description, image, price, onShowMore }) => {
   const styles = {
     imageContainer: {
       width: "100%",
       height: "200px",
-      objectFit: "cover",
     },
-    image: { objectFit: "cover", width: "100%", height: "100%" },
+    image: { objectFit: "contain", width: "100%", height: "100%" },
     descriptionContainer: {
-      display: "flex",
-      textAlign: "center",
-      alignItems: "center",
+      display: "grid",
+
+      placeItems: "center",
+      gap: 8,
+      margin: "10px 15px",
     },
     buttonHolder: {
       display: "flex",
       justifyContent: "center",
+      padding: "10px 0px",
+    },
+    ShoppingCartOutlined: {
+      fontSize: fontSize.normal,
+      margin: "0px 3px",
     },
   };
 
@@ -32,15 +39,30 @@ const ArticleCard = ({ title, description, image, price, onShowMore }) => {
         <img src={image} style={styles.image} alt={altTag} />
       </SimplifiedDiv>
       <SimplifiedDiv style={styles.descriptionContainer}>
-        <Text color={colors.accentColor} fontSize={fontSize.large}>
+        <Text
+          color={colors.accentColor}
+          fontSize={fontSize.large}
+          fontWeight={fontWeight.mediumBold}
+        >
           {price}
         </Text>
         <Text fontSize={fontSize.medium}>{title}</Text>
-        <Text fontSize={fontSize.optimal}>{description}</Text>
+        <Text color={colors.lightBlack} fontSize={fontSize.optimal}>
+          {description.length > 70
+            ? description.slice(0, 70) + "..."
+            : description}
+        </Text>
       </SimplifiedDiv>
       <SimplifiedDiv style={styles.buttonHolder}>
-        <PrimaryButton primary color={colors.accentColor} onClick={onShowMore}>
-          Show more
+        <PrimaryButton
+          primary
+          borderRadius="3px"
+          padding="10px"
+          color={colors.accentColor}
+          onClick={onShowMore}
+        >
+          <ShoppingCartOutlined style={styles.ShoppingCartOutlined} />
+          Add to cart
         </PrimaryButton>
       </SimplifiedDiv>
     </Card>
--- nitshop/src/Templates/ArticlesHomePage/ArticlesHomePage.jsx
+++ nitshop/src/Templates/ArticlesHomePage/ArticlesHomePage.jsx
@@ -2,30 +2,69 @@ import React from "react";
 import ArticleCard from "../ArticleCard/ArticleCard";
 import { Grid } from "@mui/material";
 import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
+import article1 from "../../assets/images/home/product1.jpg";
+import article2 from "../../assets/images/home/product2.jpg";
+import article3 from "../../assets/images/home/product3.jpg";
+
+const lorem =
+  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
+const articles = [
+  {
+    title: "Article 1",
+    description: lorem,
+    image: article1,
+    price: "13$",
+  },
+  {
+    title: "Article 2",
+    description: lorem,
+    image: article2,
+    price: "50$",
+  },
+  {
+    title: "Article 3",
+    description: lorem,
+    image: article3,
+    price: "63$",
+  },
+  {
+    title: "Article 4",
+    description: lorem,
+    image: article2,
+    price: "50$",
+  },
+  {
+    title: "Article 5",
+    description: lorem,
+    image: article1,
+    price: "40$",
+  },
+  {
+    title: "Article 6",
+    description: lorem,
+    image: article3,
+    price: "55$",
+  },
+];
 
 const ArticlesHomePage = () => {
   // Postaviti funkcije koje ce izlistavati artikle
   return (
     <SimplifiedDiv style={{}}>
-      <Grid container direction="row">
-        <Grid item md={4} lg={4}>
-          <ArticleCard></ArticleCard>
-        </Grid>
-        <Grid item md={4} lg={4}>
-          <ArticleCard></ArticleCard>
-        </Grid>
-        <Grid item md={4} lg={4}>
-          <ArticleCard></ArticleCard>
-        </Grid>
-        <Grid item md={4} lg={4}>
-          <ArticleCard></ArticleCard>
-        </Grid>
-        <Grid item md={4} lg={4}>
-          <ArticleCard></ArticleCard>
-        </Grid>
-        <Grid item md={4} lg={4}>
-          <ArticleCard></ArticleCard>
-        </Grid>
+      <Grid container direction="row" spacing={3}>
+        {articles.map((article) => {
+          return (
+            <Grid item md={4} lg={4}>
+              <ArticleCard
+                title={article.title}
+                description={article.description}
+                image={article.image}
+                price={article.price}
+              ></ArticleCard>
+            </Grid>
+          );
+        })}
       </Grid>
     </SimplifiedDiv>
   );
