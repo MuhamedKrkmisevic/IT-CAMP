65abc1795eb9a9487b60ec5c34b4fabf3a95589c
muhamedkrkmisevic <maska051@hotmail.com>
2023-04-26 19:54:52 +0200

    push

---
+++ nitshop/src/Templates/Footer/BottomBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
+
+const BottomBar = () => {
+  const styles = {
+    mainDiv: {
+      backgroundColor: colors.secondColor,
+      width: "100%",
+      height: "30px",
+    },
+  };
+  return <SimplifiedDiv></SimplifiedDiv>;
+};
+
+export default BottomBar;
