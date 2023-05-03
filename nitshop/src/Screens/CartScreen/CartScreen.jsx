cd691836a6921b551d4227ab0ed1e1eb227ad169
muhamedkrkmisevic <maska051@hotmail.com>
2023-05-03 19:52:28 +0200

    push

---
+++ nitshop/src/Screens/CartScreen/CartScreen.jsx
@@ -0,0 +1,18 @@
+import { Grid } from "@mui/material";
+import React from "react";
+import CustomDiv from "../../components/CustomDiv/CustomDiv";
+import Footer from "../../Templates/Footer/Footer";
+import SimplifiedDiv from "../../components/SimplifiedDiv/SimplifiedDiv";
+import Navigation from "../../Templates/Navigation/Navigation";
+import LinkBar from "../../Templates/Navigation/LinkBar";
+import TopRow from "../../Templates/Navigation/TopRow";
+
+const CartScreen = () => {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+};
+
+export default CartScreen;
