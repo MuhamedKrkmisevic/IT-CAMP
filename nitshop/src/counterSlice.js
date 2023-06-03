9907a9093006436f72624de3da79f181cf6c2a8b
muhamedkrkmisevic <maska051@hotmail.com>
2023-06-03 14:03:50 +0200

    push

--- nitshop/src/counterSlice.js
+++ nitshop/src/counterSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const counterSlice = createSlice({
   name: "counter",
   initialState: {
-    value: null,
+    value: 0,
   },
   reducers: {
     increment: (state) => {
